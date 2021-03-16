6e7a224b8c2ac9553a87abf2cff493938d4570b5
biela <jorge@gmail.com>
2021-03-16 07:01:28 -0400

    INIT PROJECT

---
+++ src/guarda/linhademontagem/GuardaEtapaProcesso.java
@@ -0,0 +1,24 @@
+package guarda.linhademontagem;
+
+public class GuardaEtapaProcesso {
+
+	private String descricao;
+	private int tempoExecucao;
+
+	public int getTempoExecucao() {
+		return tempoExecucao;
+	}
+
+	public void setTempoExecucao(int tempoExecucao) {
+		this.tempoExecucao = tempoExecucao;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+}
---
+++ src/mainclass/MainClass.java
@@ -0,0 +1,17 @@
+package mainclass;
+
+import guarda.linhademontagem.GuardaEtapaProcesso;
+import modulo.linhademontagem.LinhaDeMontagem;
+
+public class MainClass {
+	
+public static void main(String[] args) {
+		
+		LinhaDeMontagem linhaDeMontagem = new LinhaDeMontagem();
+		linhaDeMontagem.execute();
+		
+		
+		
+	}
+
+}
